d36f2cc84cb0dc106eaa0a2ab4f129d8a5370cef
Student <student@Mac-mini.local>
2015-09-15 14:34:13 -0600

    fixed issues with display elements

--- goldrush.js
+++ goldrush.js
@@ -4,7 +4,7 @@ var mainControllerFunc = function ($scope) {
 	// $scope.myHTML = <p>"I work"</p>
 	$scope.coordinates = []
  var textarray = ["I'm old", "I'm lost", "Grandma lives here", "Hardcore Donkeys Required Here", "Ain't no darn gold here", "Brrr, it's cold here", "I'm out of food", "Call 911", "This is fools gold", "OOOHHHH BOY, I'm rich", "Where's the gold", "I don't want silver", "On the big rock candy mountain", "I'm out of whiskey", "My donkey died", "Danielle found a nugget!!", "WTF", "There's Gold in Them Thar Hills!", "I've fallen and I can't get up", "There's a snake in my boot"]
-var imagearray = ["images/01.png", "images/02.png", "images/03.png", "images/04.png", "images/05.png", "images/06.png", "images/07.png", "slack-imgs.png", "images/08.png", "images/09.png", "images/10.png", "images/11.png", "images/12.png", "images/13.png", "images/14.png", "images/15.png"]
+var imagearray = ["images/01.png", "images/02.png", "images/03.png", "images/04.png", "images/05.png", "images/06.png", "images/07.png", "slack-imgs.png", "images/08.png", "images/09.png", "images/10.png", "images/11.png", "images/12.png", "images/13.png", "images/14.png", "images/15.png" ,"images/16.png"]
 	$scope.click1 = function(event) {
 	
 		console.log($scope.coordinates)
